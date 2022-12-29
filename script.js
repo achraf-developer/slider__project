4de817357d7a2c62eb21ee94dd2a2ba522e9b3b4
achraf-developer <technichome.achraf@gmail.com>
2022-12-29 17:15:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector(".active_image");
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   
+    },
+  });
+  
